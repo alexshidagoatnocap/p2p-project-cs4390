d1cf10c7ee351755519f92a531f1d9796893bfd8
ChrisVerde02 <68801011+ChrisVerde02@users.noreply.github.com>
2026-04-24 15:20:32 -0500

    Delete final_p2p_project directory

--- final_p2p_project/tracker/config.h
+++
@@ -1,9 +0,0 @@
-#ifndef TRACKER_CONFIG_H
-#define TRACKER_CONFIG_H
-
-#include "tracker.h"
-
-TrackerStatus load_tracker_config(const char *path, TrackerConfig *config);
-void print_tracker_config(const TrackerConfig *config);
-
-#endif
